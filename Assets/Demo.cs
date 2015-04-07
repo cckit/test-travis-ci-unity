b18622fe707d1184a569421e875b0a7d6f7476f6
Tommy Chan <ckchan.cs@gmail.com>
2015-04-07 10:24:25 +0800

    try try

---
+++ Assets/Demo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour
+{
+	public int health;
+
+	void Update ()
+	{
+		DemagePlayer (1);
+
+		if (health <= 0) {
+			//health = 0;
+			Destroy (gameObject);
+			Debug.Log ("Player dead! Health: " + health);
+		}
+	}
+
+	public void DemagePlayer (int demage)
+	{
+		health -= demage;
+	}
+}
---
+++ Assets/OtherDemo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OtherDemo : MonoBehaviour
+{
+	public GameObject player;
+	private Demo playerScript;
+
+	public int[] numbers;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerScript = player.GetComponent<Demo> ();
+		playerScript.DemagePlayer (10000);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (playerScript.health <= 0) {
+			print ("Other demo detected player is dead!");
+		}
+	}
+}
